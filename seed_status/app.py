0d6c7c64416039d2218c6f8a982f693487d3f32b
Gabriel Fior <fior.gbm@gmail.com>
2024-04-19 15:28:11 -0300

    Made app a bit more robust wrt Enviado em

--- seed_status/app.py
+++ seed_status/app.py
@@ -36,7 +36,9 @@ if protocolo_input:
             wrong_protocol_numbers.append(protocol_number)
 
     df = pd.DataFrame(ds)
-    df["Enviado em:"] = df["Enviado em:"].apply(process_time)
+    df["Enviado em:"] = (
+        df["Enviado em:"].apply(process_time) if "Enviado em:" in df else None
+    )
     st.dataframe(df)
 
 if wrong_protocol_numbers:
